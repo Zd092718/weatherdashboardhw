d2acb2244749b02c84410f88790a53c67b852242
Zachary Dowd <zdowd2796@gmail.com>
2021-08-15 10:50:05 -0600

    added today element

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 let apiKey = "2c10febf165600372c07befbcdd2e966";
 let searchbarEl = $('.searchbar');
 let searchInputEl = $('#city');
-let searchButtonEl = $('.search')
+let searchButtonEl = $('.search');
+let todayDisplayEl = $('.todaydisplay');
+let date = new Date();
+const month = date.getMonth();
+const day = date.getDate();
+const year = date.getFullYear();
+let today = new Date(year, month, day);
 function findWeather(city){
     let cityFind = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
 
@@ -11,14 +17,15 @@ function findWeather(city){
             throw response.json();
         }
         return response.json();
-    })
+    }
+    )
     .then(cityData => {
 
         console.log(cityData)
         var latitude = cityData.coord.lat;
         var longitude = cityData.coord.lon;
 
-        let fives = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=hourly,minutely&units=imperial&appid=${apiKey}`
+        let fives = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&units=imperial&appid=${apiKey}`
         fetch(fives)
         .then(response => {
             if(!response.ok){
@@ -29,25 +36,39 @@ function findWeather(city){
         .then(weatherData => {
             console.log(weatherData.daily)
             for (let i = 0; i < weatherData.daily.length; i++) {
-                const getDay = weatherData.daily[i];
+                const loopDays = weatherData.daily[i]
+                console.log(loopDays)
                 let date = new Date();
-                const month = date.getMonth();
-                const day = date.getDate();
-                const year = date.getFullYear();
+                const [month, day, year] = [date.getMonth(), date.getDate(), date.getFullYear()];
                 let ndate = new Date(year, month, day + i);
-                console.log(getDay)
+                console.log(ndate)
             }
+            //today element
+            var cityEl = document.createElement('h2');
+            cityEl.textContent = `${searchInputEl.val()} (${today})`
+            todayDisplayEl.append(cityEl)
+            var detailList = document.createElement('ul');
+            todayDisplayEl.append(detailList)
+            var tempEl = document.createElement('li');
+            tempEl.textContent = `Temperature: ${weatherData.daily[0].temp.day} F`
+            detailList.append(tempEl);
+            var windEl = document.createElement('li');
+            windEl.textContent = `Wind Speed: ${weatherData.daily[0].wind_speed} MPH`
+            detailList.append(windEl)
+            var humidityEl = document.createElement('li');
+            humidityEl.textContent = `Humidity: ${weatherData.daily[0].humidity} %`
+            detailList.append(humidityEl);
+            var uvindexEl = document.createElement('li');
+            uvindexEl.textContent = `UV Index: ${weatherData.daily[0].uvi}`
+            detailList.append(uvindexEl)
         })
     })
 
 }
 
 
-function displayWeather(cityResult){
-    var cityEl = document.createElement('h2');
-    var tempEl = document.createElement('p')
-    var tempEl = document.createElement('p')
-    var tempEl = document.createElement('p')
+function displayWeather(){
+
 }
 
 function handleFormSubmit(event){
@@ -55,6 +76,7 @@ function handleFormSubmit(event){
     var cityVal = searchInputEl.val();
     console.log(cityVal)
     findWeather(cityVal);
+    displayWeather();
 }
 // var cityVal = searchInputEl.value;
 // fiveDay(cityVal);
