c5fa28a89fb72ab1cdcce43ea9e7edb667f3ddd7
Zachary Dowd <zdowd2796@gmail.com>
2021-08-15 18:27:38 -0600

    weather data set

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ let searchbarEl = $('.searchbar');
 let searchInputEl = $('#city');
 let searchButtonEl = $('.search');
 let todayDisplayEl = $('.todaydisplay');
+let day1El = $('.day1');
+let day2El = $('.day2');
+let day3El = $('.day3');
+let day4El = $('.day4');
+let day5El = $('.day5');
 let date = new Date();
 const month = date.getMonth();
 const day = date.getDate();
@@ -35,39 +40,68 @@ function findWeather(city){
         })
         .then(weatherData => {
             console.log(weatherData.daily)
-            for (let i = 0; i < weatherData.daily.length; i++) {
+            //today element
+            var date1 = document.createElement('h3');
+            date1.textContent = `${month + 1}/${day}/${year}`;
+            todayDisplayEl.append(date1);
+
+            var detailList1 = document.createElement('ul');
+            todayDisplayEl.append(detailList1)
+            
+            var temp1 = document.createElement('li');
+            temp1.textContent = `Temperature: ${weatherData.daily[0].temp.day} F`;
+            detailList1.append(temp1);
+            
+            var humidity1 = document.createElement('li');
+            humidity1.textContent = `Humidity: ${weatherData.daily[0].humidity} %`
+            detailList1.append(humidity1);
+            
+            var wind1 = document.createElement('li');
+            wind1.textContent = `Wind Speed: ${weatherData.daily[0].wind_speed} MPH`
+            detailList1.append(wind1); 
+            
+            var uvi = document.createElement('li');
+            uvi.textContent = `UV Index: ${weatherData.daily[0].uvi}`
+            detailList1.append(uvi)
+
+            for (let i = 1; i < 6; i++) {
                 const loopDays = weatherData.daily[i]
+                let dayEl = $(`.day${i}`)
                 console.log(loopDays)
                 let date = new Date();
                 const [month, day, year] = [date.getMonth(), date.getDate(), date.getFullYear()];
                 let ndate = new Date(year, month, day + i);
+                const [nmonth, nday, nyear] = [ndate.getMonth(), ndate.getDate(), ndate.getFullYear()];
                 console.log(ndate)
+
+                let iconID = weatherData.daily[i].weather[0].icon
+
+                var icon = document.createElement('img');
+                icon.className = "weather-icon"
+                icon.setAttribute('src', `http://openweathermap.org/img/wn/${iconID}@2x.png`);
+                dayEl.append(icon);
+
+                var date1 = document.createElement('h3');
+                date1.textContent = `${nmonth + 1}/${nday}/${nyear}`;
+                dayEl.append(date1);
+                var detailList = document.createElement('ul');
+                dayEl.append(detailList)
+    
+                var temp = document.createElement('li');
+                temp.textContent = `Temperature: ${loopDays.temp.day} F`;
+                detailList.append(temp);
+    
+                var humidity = document.createElement('li');
+                humidity.textContent = `Humidity: ${loopDays.humidity} %`
+                detailList.append(humidity);
+    
+                var wind = document.createElement('li');
+                wind.textContent = `Wind Speed: ${loopDays.wind_speed} MPH`
+                detailList.append(wind);
             }
-            //today element
-            var cityEl = document.createElement('h2');
-            cityEl.textContent = `${searchInputEl.val()} (${today})`
-            todayDisplayEl.append(cityEl)
-            var detailList = document.createElement('ul');
-            todayDisplayEl.append(detailList)
-            var tempEl = document.createElement('li');
-            tempEl.textContent = `Temperature: ${weatherData.daily[0].temp.day} F`
-            detailList.append(tempEl);
-            var windEl = document.createElement('li');
-            windEl.textContent = `Wind Speed: ${weatherData.daily[0].wind_speed} MPH`
-            detailList.append(windEl)
-            var humidityEl = document.createElement('li');
-            humidityEl.textContent = `Humidity: ${weatherData.daily[0].humidity} %`
-            detailList.append(humidityEl);
-            var uvindexEl = document.createElement('li');
-            uvindexEl.textContent = `UV Index: ${weatherData.daily[0].uvi}`
-            detailList.append(uvindexEl)
+            
         })
     })
-
-}
-
-
-function displayWeather(){
 
 }
 
@@ -76,10 +110,10 @@ function handleFormSubmit(event){
     var cityVal = searchInputEl.val();
     console.log(cityVal)
     findWeather(cityVal);
-    displayWeather();
+    var history = JSON.parse(localStorage.getItem('history') || '[]');
+    history.push(cityVal)
+    localStorage.setItem('history', JSON.stringify(history));
+
 }
-// var cityVal = searchInputEl.value;
-// fiveDay(cityVal);
-// todayFore(cityVal);
 
 searchButtonEl.on('click', handleFormSubmit)
